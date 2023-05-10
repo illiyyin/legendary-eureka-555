2c0f87051ca3438f8c9f15a1f1dbcad01990d2cc
illiyyin <iilliyyin@gmail.com>
2023-05-10 20:39:00 +0700

    create Todos and TodoItem component

---
+++ src/components/Todos.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import TodoItem from './TodoItem' // Lakukan import
+
+const Todos = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        // Berikan datanya ke TodoItem
+        return <TodoItem key={todo.id} todo={todo} />
+      })}
+    </div>
+  )
+}
+
+export default Todos
